79b6a2c2285d1e7643fbf49f94ed231b0d6fad88
mjtakenon <takenon777@gmail.com>
2017-12-01 12:10:31 +0900

    fixed filename

--- html/index.php
+++ html/index.php
@@ -5,9 +5,9 @@
         <meta charset="utf-8"/>
         <title> CreateMyJourney TestPage </title>
         <!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.1.2/css/bulma.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
-        <script async src="http://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.1.2/css/bulma.css"> -->
+        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"> -->
+        <!-- <script async src="http://platform.twitter.com/widgets.js" charset="utf-8"></script> -->
         <!--<link href="https://fonts.googleapis.com/earlyaccess/sawarabiMincho.css" rel="stylesheet" />-->
     </head>
 
@@ -19,7 +19,7 @@
             <main>
                 phptest
                 <?php
-                    echo "hello!!!!";
+                    echo "hello";
                 ?>
             </main>
 
